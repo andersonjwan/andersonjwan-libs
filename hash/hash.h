ffcd58d354317369e018eaae5c8b4843cb98dfb0
andersonjwan <andersonjwan@outlook.com>
2020-07-25 16:00:16 -0700

    renamed data definitions

--- hash/hash.h
+++ hash/hash.h
@@ -1,16 +1,26 @@
 #ifndef HASH_H
 #define HASH_H
 
+/* standard libarary header(s) */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* RESTRICTION(S) */
+#define TABLE_SIZE 10
+
+/* data structure definition(s) */
 typedef struct {
   char *key;   // item key
   void *value; // item value
-} item;
+} hash_item;
 
 typedef struct {
   unsigned int size;  // max table size
   unsigned int count; // number of items in table currently
 
   char **items;       // array of pointers to item(s)
-} table;
+} hash_table;
+
+
 
 #endif
