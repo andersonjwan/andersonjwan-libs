9bb80ef8f4471f3ec5a0e031df78cc4d5932cffc
andersonjwan <andersonjwan@outlook.com>
2020-07-25 15:18:30 -0700

    defined hash table and item

--- hash/hash.h
+++ hash/hash.h
@@ -1,4 +1,16 @@
 #ifndef HASH_H
 #define HASH_H
 
+typedef struct {
+  char *key;   // item key
+  void *value; // item value
+} item;
+
+typedef struct {
+  unsigned int size;  // max table size
+  unsigned int count; // number of items in table currently
+
+  char **items;       // array of pointers to item(s)
+} table;
+
 #endif
